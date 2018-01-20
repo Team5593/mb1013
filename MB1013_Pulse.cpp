c113e8114fc3012f882126c42a2d0aea00b1ff11
liamkinne <kinneliam@gmail.com>
2018-01-20 23:49:49 +1100

    added pulse reading

---
+++ MB1013_Pulse.cpp
@@ -0,0 +1,27 @@
+#include "MB1013_Pulse.h"
+
+MB1013_Pulse::MB1013_Pulse(DigitalInput *input): input(input), output(output)
+{
+	output->Set(false);
+}
+
+double MB1013_Pulse::Get()
+{
+	output->Set(true); // enable sensor
+
+	while (input->Get() == false);
+
+	double start_time = Timer::GetFPGATimestamp();
+
+	while (input->Get() == true);
+
+	double end_time = Timer::GetFPGATimestamp();
+
+	double reading = end_time - start_time;
+	reading /= 1000000; // convert from seconds to uS
+	reading *= 1000; // convert from mm to meters
+
+	output->Set(false); // disable sensor
+
+	return reading;
+}
---
+++ MB1013_Pulse.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "MB1013_Base.h"
+#include <DigitalInput.h>
+#include <DigitalOutput.h>
+#include <time.h>
+
+class MB1301_Pulse: public MB1013_Base
+{
+public:
+	MB1301_Pulse(DigitalInput *input, DigitalOutput *output);
+	~MB1301_Pulse();
+
+	double Get(); // returns distance in meters
+
+private:
+	DigitalInput *input;
+	DigitalOutput *output;
+
+	double timeout = 0.2;
+};
